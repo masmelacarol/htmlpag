84626bd7b829aa5c29a37058b17737b1fa89b0c3
masmelacarol <carolstefannym@gmail.com>
2019-07-30 23:40:51 -0500

    Actualizando.....

---
+++ app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDV13girJEzB4bfZVtZR4-HuxpLVtotoh0",
+    authDomain: "html-c05d3.firebaseapp.com",
+    databaseURL: "https://html-c05d3.firebaseio.com",
+    projectId: "html-c05d3",
+    storageBucket: "",
+    messagingSenderId: "216526233047",
+    appId: "1:216526233047:web:d1024d9e3ebdd012"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
